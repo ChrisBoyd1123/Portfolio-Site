e115ae444246c6adeea5ecf972b1d3df60b84426
Christopher Boyd <chrisboyd1123@gmail.com>
2021-07-28 19:35:47 -0500

    Feat: header, nav, menu, software-bio, illustration-bio, and home-bio components initialized.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { MenuComponent } from './menu/menu.component';
+import { SoftwareBioComponent } from './software-bio/software-bio.component';
+import { IllustrationBioComponent } from './illustration-bio/illustration-bio.component';
+import { HomeBioComponent } from './home-bio/home-bio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    MenuComponent,
+    SoftwareBioComponent,
+    IllustrationBioComponent,
+    HomeBioComponent
   ],
   imports: [
     BrowserModule,
