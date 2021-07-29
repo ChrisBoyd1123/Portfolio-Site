2ea4bdf36f524d4dbbefac5ab1b08c7a24b16458
Christopher Boyd <chrisboyd1123@gmail.com>
2021-07-28 19:35:00 -0500

    Feat: Routing for home and bio pages added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SoftwareBioComponent } from './software-bio/software-bio.component';
+import { IllustrationBioComponent } from './illustration-bio/illustration-bio.component';
+import { HomeBioComponent } from './home-bio/home-bio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeBioComponent},
+  {path: 'software', component:SoftwareBioComponent},
+  {path: 'illustration', component:IllustrationBioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
