ab1f47cd7082626fd3d0590e705b87c6bd7a076b
Christopher Boyd <chrisboyd1123@gmail.com>
2021-07-28 19:38:12 -0500

    Feat: Illustration-bio page initialized and routed.

---
+++ src/app/illustration-bio/illustration-bio.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IllustrationBioComponent } from './illustration-bio.component';
+
+describe('IllustrationBioComponent', () => {
+  let component: IllustrationBioComponent;
+  let fixture: ComponentFixture<IllustrationBioComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ IllustrationBioComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IllustrationBioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/illustration-bio/illustration-bio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-illustration-bio',
+  templateUrl: './illustration-bio.component.html',
+  styleUrls: ['./illustration-bio.component.css']
+})
+export class IllustrationBioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
