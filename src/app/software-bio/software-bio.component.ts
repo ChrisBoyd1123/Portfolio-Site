23dbe5482bb6c467d1af0795892caae308e88b98
Christopher Boyd <chrisboyd1123@gmail.com>
2021-07-28 19:41:28 -0500

    Feat: Software-bio page initialized and routed.

---
+++ src/app/software-bio/software-bio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-software-bio',
+  templateUrl: './software-bio.component.html',
+  styleUrls: ['./software-bio.component.css']
+})
+export class SoftwareBioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
