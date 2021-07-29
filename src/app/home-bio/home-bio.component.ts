93a01636e7e79e3c9a947d9823e29ed00ccadfe6
Christopher Boyd <chrisboyd1123@gmail.com>
2021-07-28 19:37:44 -0500

    Feat: Home-bio page initialized and routed.

---
+++ src/app/home-bio/home-bio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-bio',
+  templateUrl: './home-bio.component.html',
+  styleUrls: ['./home-bio.component.css']
+})
+export class HomeBioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
