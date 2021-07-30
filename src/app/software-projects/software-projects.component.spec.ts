d510c8bfcdce511ba3452d2e60a8f00856201732
Christopher Boyd <chrisboyd1123@gmail.com>
2021-07-29 19:28:30 -0500

    Feat: Projects page initialized and routed.

---
+++ src/app/software-projects/software-projects.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SoftwareProjectsComponent } from './software-projects.component';
+
+describe('SoftwareProjectsComponent', () => {
+  let component: SoftwareProjectsComponent;
+  let fixture: ComponentFixture<SoftwareProjectsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SoftwareProjectsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SoftwareProjectsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/software-projects/software-projects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-software-projects',
+  templateUrl: './software-projects.component.html',
+  styleUrls: ['./software-projects.component.css']
+})
+export class SoftwareProjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
