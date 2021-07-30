070505b6b6122a2f026ab2cb0aa0909c2858451c
Christopher Boyd <chrisboyd1123@gmail.com>
2021-07-29 19:27:52 -0500

    Fix: Illustration-bio page re-named and refactored into gallery page.

---
+++ src/app/illustration-gallery/illustration-gallery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-illustration-gallery',
+  templateUrl: './illustration-gallery.component.html',
+  styleUrls: ['./illustration-gallery.component.css']
+})
+export class IllustrationBioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
