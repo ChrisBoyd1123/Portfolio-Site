78112f6798a9236b49f92f5ca234c0fb7f43e48c
Christopher Boyd <chrisboyd1123@gmail.com>
2021-07-29 19:29:09 -0500

    Fix: Software-bio page re-named and refactored into tech page.

---
+++ src/app/software-tech/software-tech.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SoftwareBioComponent } from './software-tech.component';
+
+describe('SoftwareBioComponent', () => {
+  let component: SoftwareBioComponent;
+  let fixture: ComponentFixture<SoftwareBioComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SoftwareBioComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SoftwareBioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/software-tech/software-tech.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-software-tech',
+  templateUrl: './software-tech.component.html',
+  styleUrls: ['./software-tech.component.css']
+})
+export class SoftwareBioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
