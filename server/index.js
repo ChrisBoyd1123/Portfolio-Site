468b6f0e35dbcec51f54a7e6b0b3d12c06ad580a
Christopher Boyd <chrisboyd1123@gmail.com>
2021-08-02 11:28:42 -0500

    Feat: Webapp server now initializes database tables before listening at port.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const { routes } = require('./routes.js');
+const { db } = require('./db/index');
 
 require('dotenv').config();
 
@@ -12,9 +11,10 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cookieParser());
 app.use('/', routes);
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
+db().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server listening on ${PORT}`);
+    });
+})
