9cbf0188ff828ff0a9dce55f534a923cb8735651
Christopher Boyd <chrisboyd1123@gmail.com>
2021-08-04 19:19:42 -0500

    Feat: Helper functions imported in routes.js.

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,28 @@
 //Import ExpressJS server and routing functionality.
+const { nextElementSibling } = require("domutils");
 const express = require("express");
 const router = express.Router();
 
-//Import Node path module.
-const path = require('path');
+//Import database helper functions.
+const { searchAllProject, searchAllComic, searchAllGalleryImages, searchAllTechSkills,
+        newProj, newComic, newGalleryImg, newTechSkill } = require('./db/helpers');
 
-router.get('/', (req, res) => {
-  res.status(200).send('successful GET request.')
+// * // * // * //
+//ROUTING - GET REQUESTS
+// * // * // * //
+
+router.get('/', (req, res, next) => {
+  console.log('successful GET request.');
+  next();
+})
+
+// * // * // * //
+//ROUTING - POST REQUESTS
+// * // * // * //
+
+router.post('/', (req, res, next) => {
+  console.log('successful POST request.');
+  next();
 })
 
 module.exports.routes = router;
