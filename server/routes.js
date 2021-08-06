116f0307b588489c0710923a96a5928a870fbad8
Christopher Boyd <chrisboyd1123@gmail.com>
2021-08-06 16:09:26 -0500

    Feat: Authenticated post routes now initialized for adding projects and comics.

--- server/routes.js
+++ server/routes.js
@@ -7,6 +7,9 @@ const router = express.Router();
 const { searchAllProj, searchAllComic, searchAllGalleryImages, searchAllTechSkills,
         newProj, newComic, newGalleryImg, newTechSkill } = require('./db/helpers');
 
+//Import authentication functions.
+const { checkAuth } = require('./auth');
+
 // * // * // * //
 //ROUTING - GET REQUESTS
 // * // * // * //
@@ -59,6 +62,44 @@ router.get('/Search/Comic', (req, res) => {
 router.post('/Add', (req, res, next) => {
   console.log('successful POST request.');
   next();
+})
+
+router.post('/Add/Project', (req, res, next) => {
+  if(checkAuth(req.url.split('?AuthKey=')[1])){
+    if(req.body.link
+      && req.body.description
+      && req.body.name){
+        newProj(req.body)
+        .then((data) => {
+          res.status(200).send(data);
+        })
+      }else{
+        res.status(400).send(`Check your sent data! One or more parameters were not correctly
+        labeled. Remember, a new project requires a 'link', 'description', and 'name.'`);
+      }
+  }else{
+    res.status(403).send(`Incorrect key used for POST to portfolio database.
+    Make sure that you are sending authentication as an in-query API key titled "AuthKey."`);
+  }
+})
+
+router.post('/Add/Comic', (req, res, next) => {
+  if(checkAuth(req.url.split('?AuthKey=')[1])){
+    if(req.body.tag
+      && req.body.description
+      && req.body.name){
+        newComic(req.body)
+        .then((data) => {
+          res.status(200).send(data);
+        })
+      }else{
+        res.status(400).send(`Check your sent data! One or more parameters were not correctly
+        labeled. Remember, a new project requires a 'tag', 'description', and 'name.'`);
+      }
+  }else{
+    res.status(403).send(`Incorrect key used for POST to portfolio database.
+    Make sure that you are sending authentication as an in-query API key titled "AuthKey."`);
+  }
 })
 
 module.exports.routes = router;
