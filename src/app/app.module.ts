198297318dc86d568ef0515e74f4fcd557a72ac1
Christopher Boyd <chrisboyd1123@gmail.com>
2021-08-24 18:55:39 -0500

    Feat: Angular HTTPClientModule added as import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -31,8 +33,9 @@ import { IllustrationCommissionsComponent } from './illustration-commissions/ill
     IllustrationCommissionsComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
