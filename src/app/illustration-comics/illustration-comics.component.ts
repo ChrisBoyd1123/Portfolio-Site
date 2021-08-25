d77ccc2aa62ac5e5caab0687ff6264c74e82229b
Christopher Boyd <chrisboyd1123@gmail.com>
2021-08-24 19:03:36 -0500

    Feat: Illustration/Comics page now requests and displays comic content from website server.

--- src/app/illustration-comics/illustration-comics.component.ts
+++ src/app/illustration-comics/illustration-comics.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-illustration-comics',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IllustrationComicsComponent implements OnInit {
 
-  constructor() { }
+  protected data: object = {};
+
+  constructor(private http: HttpClient,
+    private renderer: Renderer2,
+    private elem: ElementRef) {
+  }
 
   ngOnInit(): void {
+    this.http.get(`/api/Search/Comic`)
+    .subscribe((data: any) => {
+      data.forEach((dataObj: any, dataObjIndex: number) => {
+        const newEmbedScript = this.renderer.createElement('script');
+        newEmbedScript.src = "//s.imgur.com/min/embed.js";
+        newEmbedScript.charset = "utf-8";
+        this.renderer.appendChild(document.head, newEmbedScript);
+
+        const newEmbed = this.renderer.createElement('div');
+        newEmbed.innerHTML = `<blockquote class="imgur-embed-pub" lang="en" data-id="a/${dataObj.link}"  ><a href="//imgur.com/a/${dataObj.link}">${dataObj.name}</a></blockquote>`;
+        const comicDiv = this.renderer.selectRootElement('.comic-main', true);
+        this.renderer.appendChild(comicDiv, newEmbed);
+      })
+    })
   }
 
 }
