1674ea76a12a9485d83d4480366a62cb0a801c08
Christopher Boyd <chrisboyd1123@gmail.com>
2021-08-24 19:04:22 -0500

    Feat: Illustration/GalleryImage page now requests and displays image data from website server.

--- src/app/illustration-gallery/illustration-gallery.component.ts
+++ src/app/illustration-gallery/illustration-gallery.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-illustration-gallery',
@@ -7,9 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IllustrationBioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private renderer: Renderer2,
+    private elem: ElementRef) {
+  }
 
   ngOnInit(): void {
+    this.http.get(`/api/Search/GalleryImage`)
+    .subscribe((data: any) => {
+      data.forEach((dataObj: any, dataIndex: number) => {
+        console.log('data', data);
+        console.log('curDataObj', dataObj);
+        let newGalleryImage = this.renderer.createElement('img');
+        this.renderer.listen(newGalleryImage, 'click', (e: any) => {
+          this.onImgClick(e);
+        })
+        this.renderer.addClass(newGalleryImage, 'gallery-img');
+        this.renderer.setProperty(newGalleryImage, 'src', dataObj.artLink);
+        console.log('gallery main, 26',
+        this.elem.nativeElement.querySelector('.gallery-main'))
+        this.renderer.appendChild(
+          this.elem.nativeElement.querySelector('.gallery-main'),
+          newGalleryImage);
+      })
+    })
+
+    //*//*//*//
+    //LIGHTBOX
+    //*//*//*//
+    const lightbox = this.renderer.createElement('div');
+    this.renderer.addClass(lightbox, 'inactive');
+    this.renderer.setProperty(lightbox, 'id', 'lightbox');
+    this.renderer.appendChild(document.body, lightbox);
+
+    this.renderer.listen(lightbox, 'click', (e: any) => {
+      if(e.target !== e.currentTarget){
+        return;
+      }
+      console.log('become inactive')
+      this.renderer.removeClass(lightbox, 'active');
+      this.renderer.addClass(lightbox, 'inactive');
+    })
+  }
+
+  onImgClick(e: any) {
+    const lightbox = this.renderer.selectRootElement('#lightbox', true)
+    console.log('lightbox, 47', lightbox);
+
+    //LIGHTBOX EVENT
+    //When an image is clicked, the lightbox appears.
+
+    //NEXT: Add event listener to light box for exiting once clicking away from
+    //chosenImg.
+        console.log('become active');
+        this.renderer.addClass(lightbox, 'active');
+        this.renderer.removeClass(lightbox, 'inactive');
+
+        const chosenImg = this.renderer.createElement('img');
+        chosenImg.src = (e.target.attributes[2].nodeValue);
+        while (lightbox.firstChild){
+          this.renderer.removeChild(lightbox, lightbox.firstChild);
+        }
+        lightbox.appendChild(chosenImg);
   }
 
 }
