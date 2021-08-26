16576f7ac099b0834544c6b99c03f848b5a85c0e
Christopher Boyd <chrisboyd1123@gmail.com>
2021-08-26 15:59:41 -0500

    Feat: Software-Projects Angular component now dynamically renders tech projects based on server data response.

--- src/app/software-projects/software-projects.component.ts
+++ src/app/software-projects/software-projects.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-software-projects',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SoftwareProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private renderer: Renderer2,
+    private elem: ElementRef) {
+  }
 
   ngOnInit(): void {
+    this.http.get(`/api/Search/Project`)
+    .subscribe((data: any) => {
+      data.forEach((dataObj: any, dataObjIndex: number) => {
+        const projectMain = this.renderer.selectRootElement('.project-main', true);
+        const newProjectDiv = this.renderer.createElement('div');
+
+        this.renderer.addClass(newProjectDiv, 'project-div');
+
+        const newProjectTitle = this.renderer.createElement('h2');
+        newProjectTitle.innerHTML = `<a href=${dataObj.link} target='_blank'>${dataObj.name}</a>`;
+
+        const newProjectDesc = this.renderer.createElement('p');
+        newProjectDesc.innerHTML = dataObj.desc;
+
+        this.renderer.appendChild(projectMain, newProjectDiv);
+        this.renderer.appendChild(newProjectDiv, newProjectTitle);
+        this.renderer.appendChild(newProjectDiv, newProjectDesc);
+      })
+    })
   }
 
 }
