402e1075f21a7b40a178828343c6efd338a7d78b
Christopher Boyd <chrisboyd1123@gmail.com>
2021-08-26 16:01:06 -0500

    Feat: Software-Tech Angular component now dynamically renders tech skills based on server data response.

--- src/app/software-tech/software-tech.component.ts
+++ src/app/software-tech/software-tech.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-software-tech',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SoftwareBioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private renderer: Renderer2,
+    private elem: ElementRef) {
+  }
 
   ngOnInit(): void {
+    this.http.get(`/api/Search/TechSkill`)
+    .subscribe((data: any) => {
+
+      for(const property in data){
+        data[property].forEach((dataString: string, dataStrIndex: number) => {
+          const newListElem = this.renderer.createElement('li');
+          this.renderer.addClass(newListElem, `${property}-li`);
+          const newListText = this.renderer.createText(dataString);
+          const languageMain = this.renderer.selectRootElement(`.tech-${property}`, true);
+  
+          this.renderer.appendChild(languageMain, newListElem);
+          this.renderer.appendChild(newListElem, newListText);
+        })
+      }
+      /*
+      data.languages.forEach((dataString: string, dataStrIndex: number) => {
+        const newListElem = this.renderer.createElement('li');
+        this.renderer.addClass(newListElem, 'language');
+        const newListText = this.renderer.createText(dataString);
+        const languageMain = this.renderer.selectRootElement('.tech-languages', true);
+
+        this.renderer.appendChild(languageMain, newListElem);
+        this.renderer.appendChild(newListElem, newListText);
+      })
+      */
+    })
   }
 
 }
